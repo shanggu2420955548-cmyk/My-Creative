96afbc2a6d506e210bc59a5f14c7d04109578982
shanggu2420955548-cmyk <shanggu2420955548@gmail.com>
2026-06-18 13:51:37 +0800

    编程语言学习测试

---
+++ 实验visual studio 项目位置/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int age = 20;
+
+            double pi = 3.14;
+
+            float distance = 23.124F;
+        }
+    }
+}
